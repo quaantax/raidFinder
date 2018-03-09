e1dc4398b18d41ec911f0e16fa5b0baa83a6e290
Jordi <jordijedi@gmail.com>
2018-03-09 10:37:57 +0100

    login y registro

---
+++ app/src/main/java/com/example/jordi/raidfinder/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.jordi.raidfinder;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText emailLogin;
+    private EditText passwordLogin;
+    private Button loginBoton;
+    private TextView registerMainText;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MyActivity";
+
+    String email;
+    String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailLogin=(EditText) findViewById(R.id.emailLogin);
+        passwordLogin=(EditText) findViewById(R.id.passwordLogin);
+        registerMainText=(TextView) findViewById(R.id.registerMainText);
+        loginBoton=(Button) findViewById(R.id.loginBoton);
+
+        loginBoton.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
+    public void registrarUsuario(View view) {
+        Intent intent = new Intent(this, loginRegister.class);
+        startActivity(intent);
+    }
+    public void loginUsuario(View view){
+        email= String.valueOf(emailLogin.getText());
+        password=String.valueOf(passwordLogin.getText());
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/jordi/raidfinder/loginRegister.java
@@ -0,0 +1,65 @@
+package com.example.jordi.raidfinder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class loginRegister extends AppCompatActivity {
+    private EditText correoRegistro;
+    private EditText passwordRegistro;
+    private Button botonRegistro;
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
+    private static final String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        correoRegistro=(EditText) findViewById(R.id.correoRegistro);
+        passwordRegistro=(EditText) findViewById(R.id.passwordRegistro);
+        botonRegistro=(Button) findViewById(R.id.botonRegistro);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+   
+
+    public void registroUsuario(View view) {
+        email= String.valueOf(correoRegistro.getText());
+        password=String.valueOf(passwordRegistro.getText());
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
